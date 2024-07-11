e847d55f0b401d53a117019e0e541cda947cda2b
WerdyGithub <werdygithub@gmail.com>
2024-07-11 17:01:49 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'f190b217-3c62-4546-a58c-c84fd9fef819';
 
-const พร็อกซีไอพีs = ['203.78.119.205'];
+const พร็อกซีไอพีs = ['188.166.217.116'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
